2ec54962bab20310268033a90384ccd93570c925
Bastian Heist <bastian.heist@sandstorm.de>
2018-06-22 11:21:27 +0200

    TASK: Add scope annotations to repos

--- Classes/Domain/Repository/ContentRepository.php
+++ Classes/Domain/Repository/ContentRepository.php
@@ -2,7 +2,11 @@
 namespace Sandstorm\NeosH5P\Domain\Repository;
 
 use Neos\Flow\Persistence\Doctrine\Repository;
+use Neos\Flow\Annotations as Flow;
 
+/**
+ * @Flow\Scope("singleton")
+ */
 class ContentRepository extends Repository {
 
 }
