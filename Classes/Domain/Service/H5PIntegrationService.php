09bf8fdd21bab99aa6665898a1545da96f419d67
Bastian Heist <bastian.heist@sandstorm.de>
2018-06-30 15:52:44 +0200

    TASK: Finish H5P Integration Service (some TODOs left)

--- Classes/Domain/Service/H5PIntegrationService.php
+++ Classes/Domain/Service/H5PIntegrationService.php
@@ -53,6 +53,12 @@ class H5PIntegrationService
 
     /**
      * @Flow\Inject
+     * @var \H5PContentValidator
+     */
+    protected $h5pContentValidator;
+
+    /**
+     * @Flow\Inject
      * @var UserService
      */
     protected $userService;
@@ -91,12 +97,12 @@ class H5PIntegrationService
                 'H5P' => $this->h5pCore->getLocalization(),
             ],
             'hubIsEnabled' => $this->h5pFramework->getOption('hub_is_enabled') == 1,
-            'reportingIsEnabled' => $this->h5pFramework->getOption('enable_lrs_content_types') == 1
+            'reportingIsEnabled' => $this->h5pFramework->getOption('enable_lrs_content_types') == 1,
+            'core' => [
+                'scripts' => $this->getRelativeCoreScriptUrls(),
+                'styles' => $this->getRelativeCoreStyleUrls()
+            ]
         ];
-
-        // Add core JavaScript and CSS
-        $settings['core']['scripts'] = $this->getRelativeCoreScriptUrls();
-        $settings['core']['styles'] = $this->getRelativeCoreStyleUrls();
 
         // If we have a current user, pass his data to the frontend too
         if ($currentUser !== null) {
@@ -109,6 +115,40 @@ class H5PIntegrationService
         }
 
         return $settings;
+    }
+
+    /**
+     * Returns an array with a set of editor settings that the H5P JavaScript needs
+     * to do its thing.
+     *
+     * @param null $contentId provide this to set the "nodeVersionId" - needed to edit contents.
+     * @return array
+     */
+    public function getEditorSettings($contentId = null): array
+    {
+        $editorSettings = [
+            'filesPath' => $this->h5pPublicFolderUrl . $this->h5pEditorPublicFolderName,
+            'fileIcon' => [
+                'path' => $this->h5pPublicFolderUrl . $this->h5pEditorPublicFolderName . '/images/binary-file.png',
+                'width' => 50,
+                'height' => 50,
+            ],
+            'ajaxPath' => '', // TODO admin_url('admin-ajax.php?token=' . wp_create_nonce('h5p_editor_ajax') . '&action=h5p_'),
+            'libraryUrl' => '', // TODO - huh ? plugin_dir_url('h5p/h5p-editor-php-library/h5peditor.class.php'),
+            'copyrightSemantics' => $this->h5pContentValidator->getCopyrightSemantics(),
+            'assets' => [
+                'css' => $this->getRelativeEditorStyleUrls(),
+                'js' => $this->getRelativeEditorScriptUrls()
+            ],
+            'deleteMessage' => 'Are you sure you wish to delete this content?',
+            'apiVersion' => \H5PCore::$coreApi
+        ];
+
+        if($contentId !== null) {
+            $editorSettings['nodeVersionId'] = $contentId;
+        }
+
+        return $editorSettings;
     }
 
     /**
@@ -139,6 +179,51 @@ class H5PIntegrationService
         $urls = [];
         foreach (\H5PCore::$styles as $style) {
             $urls[] = $this->h5pPublicFolderUrl . $this->h5pCorePublicFolderName . '/' . $style . $this->getCacheBuster();
+        }
+        return $urls;
+    }
+
+    /**
+     * Generates the relative script urls the H5P JS expects in window.H5PIntegration.editor.assets.js.
+     * Is needed for the window.H5PIntegration object and also to actually load these scripts into
+     * the window as head scripts.
+     *
+     * @return array
+     */
+    public function getRelativeEditorScriptUrls(): array
+    {
+        $urls = [];
+        foreach (\H5peditor::$scripts as $script) {
+            $urls[] = $this->h5pPublicFolderUrl . $this->h5pEditorPublicFolderName . '/' . $script . $this->getCacheBuster();
+            /**
+             * TODO: The following is in WP, not sure if needed...
+             */
+            /**
+             * // We do not want the creator of the iframe inside the iframe
+             * if ($script !== 'scripts/h5peditor-editor.js') {
+             * $assets['js'][] = $url . $script . $cache_buster;
+             * }
+             * */
+        }
+
+        // Add language script - english only for now
+        $urls[] = $this->h5pPublicFolderUrl . $this->h5pEditorPublicFolderName . '/language/en.js';
+
+        return $urls;
+    }
+
+    /**
+     * Generates the relative style urls the H5P JS expects in window.H5PIntegration.editor.assets.css.
+     * Is needed for the window.H5PIntegration object and also to actually load these styles into
+     * the window as head styles.
+     *
+     * @return array
+     */
+    public function getRelativeEditorStyleUrls(): array
+    {
+        $urls = [];
+        foreach (\H5peditor::$styles as $style) {
+            $urls[] = $this->h5pPublicFolderUrl . $this->h5pEditorPublicFolderName . '/' . $style . $this->getCacheBuster();
         }
         return $urls;
     }
