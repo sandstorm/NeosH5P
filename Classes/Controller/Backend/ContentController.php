af01ed7b02a96a1e074e868039216eec58058504
Bastian Heist <bastian.heist@sandstorm.de>
2018-07-02 07:22:38 +0200

    TASK: First step for content creation

--- Classes/Controller/Backend/ContentController.php
+++ Classes/Controller/Backend/ContentController.php
@@ -19,8 +19,21 @@ class ContentController extends AbstractModuleController
         $coreSettings = $this->h5pIntegrationService->getCoreSettings();
         $coreSettings['editor'] = $this->h5pIntegrationService->getEditorSettings();
 
-        $this->view->assign('settings' , json_encode($coreSettings));
-        $this->view->assign('scripts' , $coreSettings['core']['scripts']);
-        $this->view->assign('styles' , $coreSettings['core']['styles']);
+        $this->view->assign('settings', json_encode($coreSettings));
+        $this->view->assign('scripts', $coreSettings['core']['scripts']);
+        $this->view->assign('styles', $coreSettings['core']['styles']);
+    }
+
+    /**
+     * @param string $title
+     * @param string $action
+     * @param string $library
+     */
+    public function createAction(string $title, string $action, string $library, string $parameters)
+    {
+        \Neos\Flow\var_dump($title);
+        \Neos\Flow\var_dump($action);
+        \Neos\Flow\var_dump($library);
+        \Neos\Flow\var_dump($parameters);
     }
 }
