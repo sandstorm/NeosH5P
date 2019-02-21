1df79aa619017b027263492539b2423ef43046f1
Bastian Heist <bastian.heist@sandstorm.de>
2019-02-21 21:02:14 +0100

    BUGFIX: use fixed master version of fileupload instead of workaround

--- Classes/Controller/Backend/EditorAjaxController.php
+++ Classes/Controller/Backend/EditorAjaxController.php
@@ -145,27 +145,8 @@ class EditorAjaxController extends ActionController
          * This call is resolved to:
          * @see \H5peditorAjax::fileUpload()
          */
-        // $this->h5pEditor->ajax->action(\H5PEditorEndpoints::FILES, 'dummy', null);
-
-        /**
-         * !!! We can't use the public API because it contains a bug in the fileUpload method
-         * where $this->storage->markFileForCleanup($file_id); is called with 1 parameter even though
-         * it expects 2. This is fixed on master, but not released yet. Therefore we make the same calls
-         * here that \H5peditorAjax::fileUpload() makes, working around the bug.
-         */
-        $file = new \H5peditorFile($this->h5pFramework);
-        if (!$file->isLoaded()) {
-            \H5PCore::ajaxError($this->h5pFramework->t('File not found on server. Check file upload settings.'));
-            return;
-        }
-
-        // Make sure file is valid and mark it for cleanup at a later time
-        if ($file->validate()) {
-            $file_id = $this->h5pCore->fs->saveFile($file, 0);
-            $this->h5pEditor->ajax->storage->markFileForCleanup($file_id, 0);
-        }
-        $file->printResult();
-
+        $this->h5pEditor->ajax->action(\H5PEditorEndpoints::FILES, 'dummy', null);
+         
         // See above
         exit;
     }
